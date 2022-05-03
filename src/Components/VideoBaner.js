3e0c14718e388cb1ad8197d4ea55d1c2569f7332
Dominika <dominika.moravicova@gmail.com>
2022-05-03 11:28:42 +0200

    uprava

--- src/Components/VideoBaner.js
+++ src/Components/VideoBaner.js
@@ -9,8 +9,8 @@ export default function VideoBaner() {
 		        <video
 		          loop
 		          muted
-		          autoplay
-		          playsinline
+		          autoPlay
+		          playsInline
 		          src="${kratke}"
 		        />,` }}>		      	
 		    </div>
