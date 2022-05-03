d2cbe2c2e01c4a05a72d960cd0b0996c4bb1298c
Dominika <dominika.moravicova@gmail.com>
2022-05-03 11:41:46 +0200

    uprava

--- src/Components/VideoBaner.js
+++ src/Components/VideoBaner.js
@@ -4,24 +4,45 @@ import kratke from '../Videos/kratke.mp4';
 export default function VideoBaner() {
 
 	return (
-		<div className='video-container container'>
-			<div dangerouslySetInnerHTML={{ __html: `
-		        <video
-		          loop
-		          muted
-		          autoPlay
-		          playsInline
-		        >
-			        <source src=${kratke} type="video/mp4" />
-			        Your browser does not support the video tag.
-		        </video>
-		        `}}>		      	
-		    </div>
+		<>
+			<div className='video-container container d-none d-sm-block'>
+				<div dangerouslySetInnerHTML={{ __html: `
+			        <video
+			          loop
+			          muted
+			          autoPlay
+			          playsInline
+			        >
+				        <source src=${kratke} type="video/mp4" />
+				        Your browser does not support the video tag.
+			        </video>
+			        `}}>		      	
+			    </div>
 
-			<div className='video-text'>
-				<p>Svadobný salón</p>
-				<h1>la novia</h1>
+				<div className='video-text'>
+					<p>Svadobný salón</p>
+					<h1>la novia</h1>
+				</div>
 			</div>
-		</div>
+
+			<div className='video-container container d-block d-sm-none'>
+				<div dangerouslySetInnerHTML={{ __html: `
+			        <video
+			          loop
+			          autoPlay
+			          playsInline
+			        >
+				        <source src=${kratke} type="video/mp4" />
+				        Your browser does not support the video tag.
+			        </video>
+			        `}}>		      	
+			    </div>
+
+				<div className='video-text'>
+					<p>Svadobný salón</p>
+					<h1>la novia</h1>
+				</div>
+			</div>
+		</>
 	)
 }
